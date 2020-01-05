3e506fb6631eaeaa7d68c611dc717feab4bfc636
attya93 <medoo.m93@gmail.com>
2020-01-05 12:38:21 +0200

    add bcryptjs to our code

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express = require("express"),
   bodyParser = require("body-parser"),
   ejs = require("ejs"),
   app = express(),
+  bcrypt = require("bcryptjs"),
   mongoose = require("mongoose"),
   encrypt = require("mongoose-encryption"),
+  md5 = require("md5"),
   port = 3000;
 
 app.use(express.static("public"));
@@ -15,6 +17,9 @@ app.use(
     extended: true
   })
 );
+
+const salt = bcrypt.genSaltSync(10);
+
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
@@ -31,11 +36,6 @@ const userSchema = new mongoose.Schema({
     require: true
   }
 });
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, {
-  secret: secret,
-  encryptedFields: ["password"]
-});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -49,21 +49,22 @@ app
     res.render("login");
   })
   .post((req, res) => {
-    User.findOne(
-      {
+    User.findOne({
         email: req.body.username
       },
       (err, user) => {
         if (err) console.log(err);
         else {
           if (user) {
-            if (user.password === req.body.password) {
-              console.log("successfuly to log-in");
-              res.render("secrets");
-            } else {
-              console.log("Password not matched");
-              res.redirect("login");
-            }
+            bcrypt.compare(req.body.password, user.password, (err, ok) => {
+              if (ok === true) {
+                console.log("successfuly to log-in");
+                res.render("secrets");
+              } else {
+                console.log("Password not matched");
+                res.redirect("login");
+              }
+            });
           }
         }
       }
@@ -76,16 +77,38 @@ app
     res.render("register");
   })
   .post((req, res) => {
-    const newUser = new User({
-      email: req.body.username,
-      password: req.body.password
-    });
-    newUser.save(err => {
-      if (!err) res.render("secrets");
-      else console.log(err);
+    bcrypt.hash(req.body.password, salt, function (err, hash) {
+      // Store hash in your password DB.
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+      });
+      newUser.save(err => {
+        if (!err) res.render("secrets");
+        else console.log(err);
+      });
     });
   });
 
 app.listen(port, () => {
   console.log("localhost:3000");
 });
+
+/**  this is leve 2 of scurity*/
+/*
+const secret = process.env.SECRET;
+userSchema.plugin(encrypt, {
+  secret: secret,
+  encryptedFields: ["password"]
+});
+*/
+/**
+ * login check to save
+   if (user.password === md5(req.body.password)) {
+              console.log("successfuly to log-in");
+              res.render("secrets");
+            } else {
+              console.log("Password not matched");
+              res.redirect("login");
+            }
+ */
